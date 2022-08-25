18a22a7ad4f643b89dafdb878852c129093381c7
Yahya El mrabti <yel-mrab@e3r4p8.1337.ma>
2022-08-25 19:26:10 +0100

    impl: header file for utils

---
+++ inc/mutils.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mutils.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/24 22:07:43 by yel-mrab          #+#    #+#             */
+/*   Updated: 2022/08/24 22:14:24 by yel-mrab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTILS_H
+#define MUTILS_H
+
+int		ft_strlen(char *str);
+int		ft_strncmp(char *s1, char *s2, int n);
+void	ft_putstr_fd(char *str, int fd);
+
+#endif
