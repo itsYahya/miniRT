99781201b5b17036b1526ef7e90e3a2e2b9df2c7
itsyahya <yahya181elmrabti@gmail.com>
2022-08-30 23:48:48 +0100

    style: removing 42header

--- src/main.c
+++ src/main.c
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/23 16:55:19 by yel-mrab          #+#    #+#             */
-/*   Updated: 2022/08/24 22:32:26 by yel-mrab         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "miniRT.h"
 #include "get_next_line.h"
 #include "mutils.h"
@@ -18,6 +6,7 @@
 
 int	main(int argc, char **argv)
 {
+	
 	if (argc != 2)
 		return (ft_putstr_fd("invalid argumanet\n", 2), 1);
 	parser(argv[1]);
