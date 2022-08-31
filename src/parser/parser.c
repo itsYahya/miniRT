b7643454fe575b5317ec1f4781a47b6fad2adaae
itsyahya <yahya181elmrabti@gmail.com>
2022-08-31 22:07:56 +0100

    fix: parser logic

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -2,15 +2,59 @@
 #include "mutils.h"
 #include "parser.h"
 #include <fcntl.h>
+#include <stdio.h>
+#include "sceneMaterial.h"
+
+static int	identifier(char	*id)
+{
+	if (ft_strncmp(id, "A", 1) == 0)
+		return ('A');
+	if (ft_strncmp(id, "C", 1) == 0)
+		return ('C');
+	if (ft_strncmp(id, "L", 1) == 0)
+		return ('L');
+	if (ft_strncmp("pl", id, 2) == 0)
+		return ('P');
+	if (ft_strncmp("sp", id, 2) == 0)
+		return ('S');
+	if (ft_strncmp("cy", id, 2) == 0)
+		return ('Y');
+	if (ft_strncmp("\n", id, 1) == 0 )
+		return ('N');
+	return (-1);
+}
+
+static void	parseElements(char **tokens, int id)
+{
+	if (id == ambLightning)
+		parseAmbLightning(tokens);
+	else if (id == camera)
+		parseCamera(tokens);
+	else if (id == light)
+		parseLight(tokens);
+	else if (id == sphere)
+		parseSphere(tokens);
+	else if (id == plane)
+		parsePlane(tokens);
+	else
+		parseCylinder(tokens);
+}
 
 static int	ft_read(int fd)
 {
 	char	*line;
+	char	**tokens;
+	int		id;
 
 	line = get_next_line(fd);
 	while (line)
 	{
-		printf("%s", line);
+		tokens = ft_split(line, ' ');
+		id = identifier(tokens[0]);
+		if (id < 0)
+			exit(1);
+		if (id != 'N')
+			parseElements(tokens, id);
 		line = get_next_line(fd);
 	}
 	return (0);
