9233cd895d6178a749eac35a05b8cfab573f62d9
itsyahya <yahya181elmrabti@gmail.com>
2022-09-02 00:08:12 +0100

    fix: small fix

--- src/parser/sceneObjects.c
+++ src/parser/sceneObjects.c
@@ -13,3 +13,8 @@ void	parseCylinder(char **tokens, t__data *data)
 	data->camera.fov = 8;
 	printf("here we go again cylinder %s\n", tokens[0]);
 }
+
+void	parsePlane(char **tokens, t__data *data){
+	(void)tokens;
+	(void)data;
+}
