95e0fdfa044226324bcec12b4e0db02812e5452b
mzarhou <mzarhou.cc@gmail.com>
2022-09-07 05:53:07 +0100

    refactor: matrix multiply

--- src/matrix/matrix_multiply.c
+++ src/matrix/matrix_multiply.c
@@ -1,27 +1,7 @@
 #include "matrix.h"
 #include "tuple.h"
 
-static float	calc_value(t_matrix mt1, t_matrix mt2, int x, int y)
-{
-	t_tuple	row;
-	t_tuple col;
-
-	row = tuple(
-		mt1.raw[y][0],
-		mt1.raw[y][1],
-		mt1.raw[y][2],
-		mt1.raw[y][3]
-	);
-	col = tuple(
-		mt2.raw[0][x],
-		mt2.raw[1][x],
-		mt2.raw[2][x],
-		mt2.raw[3][x]
-	);
-	return dot(row, col);
-}
-
-t_matrix	ft_matrix_multiply(t_matrix mt1, t_matrix mt2)
+t_matrix	matrix_multiply(t_matrix mt1, t_matrix mt2)
 {
 	t_matrix	result;
 	int			x;
@@ -32,7 +12,7 @@ t_matrix	ft_matrix_multiply(t_matrix mt1, t_matrix mt2)
 	{
 		x = -1;
 		while (++x < 4)
-			result.raw[y][x] = calc_value(mt1, mt2, x, y);
+			result.raw[y][x] = dot(get_row(mt1, y), get_col(mt2, x));
 	}
 	return (result);
 }
