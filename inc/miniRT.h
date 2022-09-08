55e244f61a2a4bc4c8196a5f45691f19381697eb
itsyahya <yahya181elmrabti@gmail.com>
2022-09-09 00:55:19 +0100

    feat: implt of mlx events

--- inc/miniRT.h
+++ inc/miniRT.h
@@ -24,4 +24,6 @@ void	ft_solve_sphere(const t_ray ray, t_sphere sphere, float *t, t_color *color)
 void	ft_setup_camera(t_vcamera *vcamera, const t_camera camera);
 void	ft_setray(const t_vcamera camera, t_ray *ray, t_pair pair);
 
+//mlx events functions
+void	ft_setup_mlxevnets(void *win, t__data *data, t_canvas canvas);
 #endif
--- src/canvas/canvas.c
+++ src/canvas/canvas.c
@@ -1,4 +1,5 @@
 #include "canvas.h"
+#include "miniRT.h"
 
 void		ft_write_pixel(t_canvas c, int x, int y, t_color color)
 {
@@ -22,6 +23,7 @@ t_canvas	ft_canvas(float width, float height)
 	canvas.mlx_win = mlx_new_window(canvas.mlx_ptr, width, height, "Hello world!");
 	canvas.img = mlx_new_image(canvas.mlx_ptr, width, height);
 	canvas.img_data = (uint32_t *)mlx_get_data_addr(canvas.img, &temp, &temp, &temp);
+	ft_setup_mlxevnets(canvas.mlx_win, 0, canvas);
 	return (canvas);
 }
 
