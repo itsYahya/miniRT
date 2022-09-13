771ac4329fbc5d819fbbce06dd5fc9a5fa1d4abd
itsyahya <yahya181elmrabti@gmail.com>
2022-09-13 17:58:23 +0100

    chore: shading test

--- src/raytracing/randerer_.c
+++ src/raytracing/randerer_.c
@@ -22,15 +22,49 @@ void	ft_look_inters(t_list *head, t_ray ray, t_info *info)
 	}
 }
 
+t_tuple	vectotuple(t_vect3 vec)
+{
+	return (tuple(vec.x, vec.y, vec.z, 0));
+}
+
+void	ft_shading(t__data *data, t_info *info)
+{
+	t_tuple	v_light;
+	t_tuple	p_light;
+	t_ray	ray;
+	t_info	shade;
+	float	d;
+
+	p_light = vectotuple(data->light.point);
+	v_light = subst_tuple(p_light, info->point);
+	ray.direction = v_light;
+	ray.origin = info->point;
+	shade.t = -1;
+	d = (dot(normalize(v_light), info->normal));
+	if (d < 0)
+		d = 0;
+	ft_look_inters(data->objects, ray, &shade);
+	if (shade.t == -1)
+	{
+		info->color.r = info->color.r * d + (255*0.2 *d);
+		info->color.g = info->color.g * d + (255*0.2*d);
+		info->color.b = info->color.b * d + (255*0.2*d);
+	}
+	else
+		info->color.raw = 0;
+}
+
 static void	per_pixel(const t_pair pair, t_canvas canvas, t_vcamera vcamera, t__data *data)
 {
 	t_ray		ray;
 	t_info		info;
 
 	ft_setray(vcamera, &ray, pair);
-	info.color.raw = 0x00ffffff;
+	info.color.raw = 0x00ffaaff;
 	info.t = -1;
 	ft_look_inters(data->objects, ray, &info);
+	if (info.t != -1)
+		ft_shading(data, &info);
 	ft_write_pixel(canvas, pair.x, pair.y, info.color);
 }
 
