eff3014f63a007cddd3c2abce333ac718361da0c
itsyahya <yahya181elmrabti@gmail.com>
2022-09-16 13:13:23 +0100

    fix: ft_map_color now return a tuple with values between 0 and 1

--- src/color/ft_map_color.c
+++ src/color/ft_map_color.c
@@ -1,11 +1,11 @@
 #include "color.h"
 
-t_color	ft_map_color(t_color color)
+t_tuple	ft_map_color(t_color color)
 {
-	t_color	res;
+	t_tuple	res;
 
-	res.r = color.r / 255;
-	res.g = color.g / 255;
-	res.b = color.b / 255;
+	res.x = color.r / 255;
+	res.y = color.g / 255;
+	res.z = color.b / 255;
 	return (res);
 }
