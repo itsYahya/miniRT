4db2d8d55c084c6bcd6c578ed7ace6ffa80238a5
itsyahya <yahya181elmrabti@gmail.com>
2022-10-03 17:07:17 +0100

    feat: add the keys to exit

--- src/mlx_events/events.c
+++ src/mlx_events/events.c
@@ -6,7 +6,7 @@
 /*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 16:08:44 by yel-mrab          #+#    #+#             */
-/*   Updated: 2022/10/03 16:09:44 by yel-mrab         ###   ########.fr       */
+/*   Updated: 2022/10/03 17:05:10 by yel-mrab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,13 @@ static int	on_close(void *data)
 	return (exit(0), 0);
 }
 
+static int	on_key_press(int key, void *data)
+{
+	if (key == 53 || key == 12)
+		on_close(data);
+	return (0);
+}
+
 void	setup_mlxevents(t__data *data)
 {
 	if (! data || ! data->canvas.mlx_win)
@@ -30,4 +37,5 @@ void	setup_mlxevents(t__data *data)
 		return ;
 	}
 	mlx_hook(data->canvas.mlx_win, 17, 0, on_close, data);
+	mlx_hook(data->canvas.mlx_win, 2, 0, on_key_press, data);
 }
