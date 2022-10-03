f7403e5692dcf61e5c45d867c96f0e1adbd6bb48
itsyahya <yahya181elmrabti@gmail.com>
2022-10-03 15:59:00 +0100

    chore: norm canvas

--- src/canvas/canvas.h
+++ src/canvas/canvas.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   canvas.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/03 15:44:21 by yel-mrab          #+#    #+#             */
+/*   Updated: 2022/10/03 15:44:22 by yel-mrab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CANVAS_H
 # define CANVAS_H
 
@@ -21,7 +33,7 @@ struct s_canvas
 int			ft_canvas(t_canvas *canvas, float width, float height);
 void		destroy_canvas(t_canvas canvas);
 void		ft_write_pixel(t_canvas c, int x, int y, t_color color);
-uint32_t*	ft_pixel_at(t_canvas c, int x, int y);
+uint32_t	*ft_pixel_at(t_canvas c, int x, int y);
 void		ft_show_canvas(t_canvas canvas);
 
 #endif
