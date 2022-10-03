de950653d3ab49f48ecfc4648cd23bcfb007c1de
itsyahya <yahya181elmrabti@gmail.com>
2022-10-03 16:10:45 +0100

    chore: norm mkx_events

--- src/mlx_events/events.c
+++ src/mlx_events/events.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   events.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/03 16:08:44 by yel-mrab          #+#    #+#             */
+/*   Updated: 2022/10/03 16:09:44 by yel-mrab         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "parser_.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include "canvas.h"
 #include "miniRT.h"
 
-static int onClose(void *data)
+static int	on_close(void *data)
 {
 	ft_destroy(data);
 	return (exit(0), 0);
@@ -17,5 +29,5 @@ void	setup_mlxevents(t__data *data)
 		printf("failed to setup mlx events\n");
 		return ;
 	}
-	mlx_hook(data->canvas.mlx_win, 17, 0, onClose, data);
+	mlx_hook(data->canvas.mlx_win, 17, 0, on_close, data);
 }
